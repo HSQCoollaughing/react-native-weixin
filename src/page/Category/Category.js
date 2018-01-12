a1861098a2295f0ec6189551194e85c0c52f6cf7
橘子 <juziexe@gmail.com>
2018-01-12 18:06:04 +0800

    添加别名
    用 babel-plugin-module-resolver，配置全局别名

--- src/page/Category/Category.js
+++ src/page/Category/Category.js
@@ -4,11 +4,13 @@ import ListIconItem from './ListIconItem'
 
 export default class Category extends React.Component {
   render () {
-    return <View>
-      <ListIconItem icon={require('../../assets/imgs/ic_new_friend.png')} text='新的朋友'/>
-      <ListIconItem icon={require('../../assets/imgs/ic_group_cheat.png')} text='群聊'/>
-      <ListIconItem icon={require('../../assets/imgs/ic_tag.png')} text='标签'/>
-      <ListIconItem icon={require('../../assets/imgs/ic_offical.png')} text='公众号'/>
-    </View>
+    return (
+      <View>
+        <ListIconItem icon={require('src/assets/imgs/ic_new_friend.png')} text='新的朋友'/>
+        <ListIconItem icon={require('src/assets/imgs/ic_group_cheat.png')} text='群聊'/>
+        <ListIconItem icon={require('src/assets/imgs/ic_tag.png')} text='标签'/>
+        <ListIconItem icon={require('src/assets/imgs/ic_offical.png')} text='公众号'/>
+      </View>
+    )
   }
 }
