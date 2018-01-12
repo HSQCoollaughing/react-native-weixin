1ffa977d513a6a095b893ec5abfb559fb197d292
橘子 <juziexe@gmail.com>
2018-01-12 16:34:14 +0800

    Revert "item 图标组件"
    This reverts commit 9e10470024bb2a03f3e4a455b8d2c952505057d6.

--- src/page/Category/Category.js
+++ src/page/Category/Category.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import { View, Text, FlatList } from 'react-native'
-import ListIconItem from './ListIconItem'
+import { View, Text } from 'react-native'
 
 export default class Category extends React.Component {
-  render () {
-    return <View>
-      <ListIconItem icon={require('../../assets/imgs/ic_new_friend.png')} text='新的朋友'/>
-      <ListIconItem icon={require('../../assets/imgs/ic_group_cheat.png')} text='群聊'/>
-      <ListIconItem icon={require('../../assets/imgs/ic_tag.png')} text='标签'/>
-      <ListIconItem icon={require('../../assets/imgs/ic_offical.png')} text='公众号'/>
-
-
-    </View>
+  render(){
+    return <Text>
+      Category
+    </Text>
   }
 }
