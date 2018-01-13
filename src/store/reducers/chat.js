90476fc462787c6c9a914df9c720fbaec576a7eb
橘子 <juziexe@gmail.com>
2018-01-13 18:42:03 +0800

    删除测试代码

--- src/store/reducers/chat.js
+++ src/store/reducers/chat.js
@@ -7,19 +7,15 @@ const initState = {
       msgText: '今天吃啥',
       date: '11:30'
     }
-  ],
-  id: 1
+  ]
 }
 
 const CHAT_ADD_MSG = 'CHAT_ADD_MSG'
-const CHAT_ADD_NUM = 'CHAT_ADD_NUM'
 
 export function chat (state = initState, action) {
   switch (action.type) {
     case CHAT_ADD_MSG:
-      return {...state, msgs: state.msgs.concat([action.payload])}
-    case CHAT_ADD_NUM:
-      return {...state, id: action.id}
+      return {...state, msgs: state.msgs.concat(action.payload)}
     default:
       return state
   }
@@ -32,9 +28,3 @@ export function addMsg (msgData) {
   }
 }
 
-export function addNum (id) {
-  return {
-    type: CHAT_ADD_NUM,
-    id: id
-  }
-}
